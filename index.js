4e003827a1c3f14836872760a3dfc27be0852ae0
Lenka Koubková <lvseteckova@gmail.com>
2025-04-19 10:39:32 +0200

    vložení funkce pro zapínání a vypínání žárovky

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log('funguju!');
+//rozsvěcení žárovky
+
+/*const bulbElm = document.querySelector(".bulb");
+document.addEventListener("keydown", () => {
+  bulbElm.classList.add("bulb--on");
+});*/
+
+//rozsvěcení a zhasínání žárovky
+
+/*const bulbElm = document.querySelector(".bulb");
+document.addEventListener("keydown", () => {
+  if (bulbElm.classList.contains("bulb--on")) {
+    bulbElm.classList.remove("bulb--on");
+  }else {
+    bulbElm.classList.add("bulb--on");
+  }
+});*/
+
+ 
+
+//jednodušší rozsvícení a zhasínání žárovky
+
+const bulbElm = document.querySelector(".bulb");
+document.addEventListener("keydown",() => {
+bulbElm.classList.toggle("bulb--on");
+});
